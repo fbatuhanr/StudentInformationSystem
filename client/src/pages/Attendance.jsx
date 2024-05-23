e2c3a37e38c29349e1c7384c701b63d22f505ecc
Batuhan Ozturk <fbatuhanr@gmail.com>
2024-05-23 09:34:47 +0300

    react qr

--- client/src/pages/Attendance.jsx
+++ client/src/pages/Attendance.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import QRCode from 'react-qr-code'
+import { serverAddress } from '../settings'
 
 const Attendance = () => {
   return (
-    <div className="text-center mt-12">Coming soon...</div>
+    <>
+      <div className="py-10">
+        <h2 className="text-[1.7rem] font-bold text-center">Student Attendance</h2>
+        <div className="h-0.5 mt-1 bg-[#cccccc] w-96 mx-auto"></div>
+      </div>
+
+      <div className="px-4 py-8">
+        <div className="md:w-1/2 mx-auto mb-4">
+          <QRCode value={`${serverAddress}/attendance`} />
+        </div>
+
+      </div>
+
+      <div className="absolute top-32 -left-16">
+        <img src={null} className="w-48" />
+      </div>
+    </>
   )
 }
 
