06205311e4745293b4a90201af226ebc0fbedba1
Batuhan Ozturk <fbatuhanr@gmail.com>
2024-06-03 21:08:37 +0300

    vercel

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log("listening")
 })
 
